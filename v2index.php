27418f9a7838d3054e75b4fa5e618c52aa74bec1
Nini Yen <yensydney@gmail.com>
2024-06-28 14:15:55 -0700

    comment

--- v2index.php
+++ v2index.php
@@ -28,22 +28,28 @@
         <input type="Password" name="password1" required><br>
         <input type="submit" name="submit" value="Submit">
     </form> -->
+    
 </body>
 </html>
 <?php
+    include('database.php');
+
     echo 'hi';
+
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        echo 'inside';
-        $firstname = filter_input(INPUT_POST, "firstname", FILTER_SANITIZE_SPECIAL_CHARS);
-        $lastname = filter_input(INPUT_POST, "lastname", FILTER_SANITIZE_SPECIAL_CHARS);
-        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
 
-        // $hash = password_hash($password, PASSWORD_DEFAULT);
-        // mysqli_query(mysqli $mysql, string $query, int $result_mode = MYSQLI_STORE_RESULT): mysqli_result|bool
-        // echo 'inside1';
+        echo 'Got POST. go inside';
 
-        // why does this make the script quit????
+        // $firstname = filter_input(INPUT_POST, "firstname", FILTER_SANITIZE_SPECIAL_CHARS);
+        // $lastname = filter_input(INPUT_POST, "lastname", FILTER_SANITIZE_SPECIAL_CHARS);
+        // $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        // $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        // // $hash = password_hash($password, PASSWORD_DEFAULT);
+        // // mysqli_query(mysqli $mysql, string $query, int $result_mode = MYSQLI_STORE_RESULT): mysqli_result|bool
+        // // echo 'inside1';
+
+        // // why does this make the script quit????
         // $sql = "SELECT * FROM Student where Username like $username";
         // $result = mysqli_query($conn, $sql); // WHY DOES THIS LINE CAUSE AN ERROR???
         // $resultarray = mysqli_fetch_all($result);
@@ -53,24 +59,24 @@
         //     echo "That username is taken. Try again.";
         // }
 
-        // else {
-        //     $sql = "INSERT INTO Student (FirstName, LastName, Username, Password)
-        //             VALUES ('$firstname', '$lastname', '$username', '$password')";
-        //     mysqli_query($conn, $sql);
-        //     echo"You are now registered!";
-        // }
-        // }
+        // // else {
+        // //     $sql = "INSERT INTO Student (FirstName, LastName, Username, Password)
+        // //             VALUES ('$firstname', '$lastname', '$username', '$password')";
+        // //     mysqli_query($conn, $sql);
+        // //     echo"You are now registered!";
+        // // }
+        // // }
 
-        $sql = "SELECT * FROM Student";
-        $result = mysqli_query($conn, $sql);
-        $finalarray = mysqli_fetch_all($result);
-        foreach($finalarray as $array) {
-            foreach($array as $item) {
-                echo $item . ' ';
-            }
-            echo "<br>";
-        }
-        echo 'done';
+        // $sql = "SELECT * FROM Student";
+        // $result = mysqli_query($conn, $sql);
+        // $finalarray = mysqli_fetch_all($result);
+        // foreach($finalarray as $array) {
+        //     foreach($array as $item) {
+        //         echo $item . ' ';
+        //     }
+        //     echo "<br>";
+        // }
+        // echo 'done';
 
         
     }
